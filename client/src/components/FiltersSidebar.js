76041a2128cc46986e5f6729521eafc6fdde6586
Vandenynas <stlo.subs@gmail.com>
2022-08-13 17:37:51 +0300

    Implemented timestamp filter

--- client/src/components/FiltersSidebar.js
+++ client/src/components/FiltersSidebar.js
@@ -10,15 +10,17 @@ import {
     scanDataSelector,
 } from '../slices/scanData';
 
-const FiltersSidebar = () => {
+const FilterForm = (props) => {
+    const { filteredFields, numericalOperators } = useSelector(scanDataSelector);
+
     const dispatch = useDispatch();
 
-    const { fields, filteredFields, numericalOperators } = useSelector(scanDataSelector);
 
-    const handleFilter = (event, fieldKey) => {
+    const handleFilter = (event, fieldKey, dateKey) => {
         dispatch(updateFilter(
             event.target.value,
-            fieldKey
+            fieldKey,
+            dateKey
         ))
     };
 
@@ -28,6 +30,69 @@ const FiltersSidebar = () => {
             fieldKey
         ))
     };
+
+    if (!props.field.isTimestamp) {
+        return (
+            <div className="row collapse justify-content-center gx-2 mt-2" id={`dashboard-collapse-${props.index}`}>
+                <div className="col" >
+                    <input
+                        type="text"
+                        className="form-control"
+                        // placeholder={`Filter by ${field.name} ...`}
+                        id={`filter-${props.fieldKey.toLowerCase()}`}
+                        onChange={(event) => handleFilter(event, props.fieldKey)}
+                        value={filteredFields[props.fieldKey].value}
+                    />
+                </div>
+                {props.field.isNumerical ?
+                    <div className="col-auto">
+                        <select
+                            className="form-control form-select form-select-sm"
+                            aria-label="Array select"
+                            id={`array-type-${props.index}`}
+                            defaultValue={numericalOperators[0]}
+                            onChange={(event) => handleOperatorChange(event, props.fieldKey)}>
+                            {numericalOperators.map((operator, operatorIndex) => {
+                                return <option key={operatorIndex} value={operator}>{operator}</option>
+                            })}
+                        </select>
+                    </div>
+                    : ''}
+            </div>
+        )
+    } else {
+        return (
+            <div className="row collapse justify-content-center gx-2 mt-2" id={`dashboard-collapse-${props.index}`}>
+                <div>
+                    <label htmlFor="from-date">Date from:</label>
+                    <input
+                        type="date"
+                        className="form-control"
+                        name="from-date"
+                        id={`date-from-${props.fieldKey.toLowerCase()}`}
+                        onChange={(event) => handleFilter(event, props.fieldKey, 'dateFrom')}
+                        value={filteredFields[props.fieldKey].dateFrom}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="until-date">Date until:</label>
+                    <input
+                        type="date"
+                        className="form-control"
+                        name="until-date"
+                        id={`date-${props.fieldKey.toLowerCase()}`}
+                        onChange={(event) => handleFilter(event, props.fieldKey, 'dateUntil')}
+                        value={filteredFields[props.fieldKey].dateUntil}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+const FiltersSidebar = () => {
+
+    const { fields, filteredFields } = useSelector(scanDataSelector);
 
     const filterableFields = Object.keys(filteredFields);
 
@@ -50,32 +115,7 @@ const FiltersSidebar = () => {
                                 >
                                     {field.name}
                                 </button>
-                                <div className="row collapse justify-content-center gx-2 mt-2" id={`dashboard-collapse-${index}`}>
-                                    <div className="col" >
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            // placeholder={`Filter by ${field.name} ...`}
-                                            id={`search-${field.name.toLowerCase()}`}
-                                            onChange={(event) => handleFilter(event, fieldKey)}
-                                            value={filteredFields[fieldKey].value}
-                                        />
-                                    </div>
-                                    {field.isNumerical ?
-                                        <div className="col-auto">
-                                            <select
-                                                className="form-control form-select form-select-sm"
-                                                aria-label="Array select"
-                                                id={`array-type-${index}`}
-                                                defaultValue={numericalOperators[0]}
-                                                onChange={(event) => handleOperatorChange(event, fieldKey)}>
-                                                {numericalOperators.map((operator, operatorIndex) => {
-                                                    return <option key={operatorIndex} value={operator}>{operator}</option>
-                                                })}
-                                            </select>
-                                        </div> 
-                                    : ''}
-                                    </div>
+                                <FilterForm field={field} fieldKey={fieldKey} index={index} />
                             </div>
                         </li>
                     )
