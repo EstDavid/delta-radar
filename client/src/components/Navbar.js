04282560d6beae6b364e082302c4d788b3bd76a7
Vandenynas <stlo.subs@gmail.com>
2022-08-23 21:21:24 +0300

    Implemented nav links and blockchain selector

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import OnboardingButton from './Onboarding';
+import { switchBlockchain, scanDataSelector } from '../slices/scanData';
 
 const Navbar = () => {
-
   const dispatch = useDispatch();
 
-  const symbolAll = `ALL`;
+  const {
+    blockchainSelection,
+    blockchainParameters,
+  } = useSelector(scanDataSelector);
 
-  return(
+  const handleBlockchainChange = (event) => {
+    if(blockchainSelection !== event.target.value) dispatch(switchBlockchain(event.target.value));
+  }
+
+  return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark text-white pb-2" aria-label="UniCharts navbar">
       <div className="container-fluid">
         <a href="/" className="navbar-brand text-white text-decoration-none me-md-auto">
@@ -17,21 +24,44 @@ const Navbar = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="collapse navbar-collapse ms-5" id="navbarsExample03">
+          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+            <li className="nav-item">
+              <NavLink 
+                to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link inactive')}
+              >Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink 
+              to="/Tables" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link inactive')}
+              >Tables</NavLink>
+            </li>
+          </ul>
+        </div>
 
         <div className="collapse navbar-collapse" id="navbar-menu">
-          <div className="navbar-nav ms-auto flex-nowrap mt-3 mt-md-0">
-            <OnboardingButton />
+          <div className="ms-auto d-flex mt-3 mt-md-0">
+            <div className="me-3">
+              <select className="form-select" aria-labelledby="Blockchain Selection" onChange={handleBlockchainChange}>
+                {Object.keys(blockchainParameters).map((blockchainKey, index) => {
+                  return (
+                    <option key={index} className="dropdown-item" value={blockchainKey}>{blockchainKey}</option>
+                  )
+                })}
+              </select>
+            </div>
+
             <a
               role="button"
               className="btn btn-outline-light"
-              href="https://github.com/Vandenynas/uniswapv3-charts"
+              href="https://github.com/EstDavid/delta-radar"
               target="_blank"
               rel="noreferrer noopener"
-            ><i className="bi bi-github"></i>View on Github</a>
+            ><i className="bi bi-github me-2"></i>View on Github</a>
           </div>
         </div>
       </div>
-    </nav> 
+    </nav>
   )
 }
 
