2c93ba7b669402013611277b709c412c005e70af
Vandenynas <stlo.subs@gmail.com>
2022-08-23 21:02:22 +0300

    Implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import React  from 'react';
-import Navbar from './components/Navbar';
-import ScanTable from './components/ScanTable';
-import FiltersSidebar from './components/FiltersSidebar';
+import React, { useEffect }  from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { 
+  fetchScans,
+  fetchData,
+  switchSortingEnabling,
+  changeSortedField,
+  scanDataSelector,
+} from './slices/scanData';
+import HomePage from './pages/HomePage';
+import TablePage from './pages/TablePage';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const {
+    blockchainSelection,
+    folderDatesObject,
+    aggregateCategoryFolders,
+  } = useSelector(scanDataSelector);
+
+  useEffect(() => {
+    dispatch(fetchScans(blockchainSelection));
+
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchScans(blockchainSelection));
+
+  }, [blockchainSelection]);
+
+  useEffect(() => {
+      dispatch(fetchData(blockchainSelection, folderDatesObject, aggregateCategoryFolders));
+  }, [folderDatesObject]);
+
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-3">
-            <FiltersSidebar />
-          </div>
-          <div className="col-sm-9">
-            <ScanTable />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Routes >
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Tables" element={<TablePage />} />
+    </Routes>
   );
 }
 
