9c939056b8df4ea66c20ca722fdb3dc18e701b05
Vandenynas <stlo.subs@gmail.com>
2023-02-18 16:40:55 +0100

    Initial commit

---
+++ client/src/components/FetchingData.js
@@ -0,0 +1,11 @@
+const FetchingData = () => {
+    return (
+        <div className="d-flex w-100 align-items-center justify-content-between p-2">
+            <div className="spinner-border text-secondary" role="status">
+                <span className="visually-hidden">Loading data...</span>
+            </div>
+        </div>
+    )
+}
+
+export default FetchingData
